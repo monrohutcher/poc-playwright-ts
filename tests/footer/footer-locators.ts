0df7cbfaa0d8e221470b4b48cd1617abe0b2dc16
monrohutcher <moroncsikmark@gmail.com>
2025-02-04 22:52:27 +0100

    feat(footer): add footer tests

--- tests/footer/footer-locators.ts
+++
@@ -1,5 +0,0 @@
-
-export const FOOTER_PLAN_HEADER = "99b34fdc-80dd-4b0e-a9c9-baab8c6bf5cd";
-export const FOOTER_PLAN_SECTION = "45f4d4d8-fef6-46c7-b4b7-2fac5beaad69";
-export const FOOTER_QUICK_LINKS = "bfac11be-4608-4f98-af14-eb4f8bf585f6";
-export const FOOTER_LEGAL_SECTION = "0ccc8c70-b8ff-40f3-ae78-3b0491e47ab4";
--- tests/footer/footer.spec.ts
+++ tests/footer/footer.spec.ts
@@ -1,20 +1,125 @@
 import { test, expect } from '@playwright/test';
-import { FOOTER_PLAN_HEADER, FOOTER_PLAN_SECTION } from './footer-locators';
+import { FooterLocators } from './footer.locator';
 
-test.beforeEach(async ({ page }) => {
-    await page.goto('https://www.revolut.com/');
-    await page.getByRole('button', {name: "Accept all"}).click();
-  });
+const url = "https://www.revolut.com/";
 
 
 test.describe("Footer - Plan section tests", () => {
-    test("Check the title of the Plan section", async ({ page }) => {
-        await expect(page.getByTestId(FOOTER_PLAN_HEADER)).toHaveText("Choose your plan");
+
+    test("Test the title of the Plan section", async ({ page }) => {
+        await test.step(`Navigate to ${url}.`, async () => {
+            await page.goto('/');
+            await page.getByRole('button', {name: "Accept all"}).click();
+        });
+        await test.step('Check the Plan section in footer.', async () => {
+            await expect(page.getByTestId(FooterLocators.FOOTER_PLAN_HEADER)).toBeVisible();
+            await expect(page.getByTestId(FooterLocators.FOOTER_PLAN_HEADER)).toHaveText("Choose your plan");
+        });
     });
 
-    test("Check the Standard Plan card", async ({ page }) => {
-        await expect(page.getByTestId(FOOTER_PLAN_SECTION).getByRole('listitem').filter({ hasText: 'Standard'})).toBeVisible();
-        await page.getByTestId(FOOTER_PLAN_SECTION).getByRole('listitem').filter({ hasText: 'Standard'}).click();
-        await expect(page).toHaveURL(/.*a-radically-better-account/);
+    test("Test the Standard Plan card", async ({ page }) => {
+        await test.step(`Navigate to ${url}.`, async () => {
+            await page.goto('/');
+            await page.getByRole('button', {name: "Accept all"}).click();
+        });
+        await test.step('Check the Standard Plan card in footer', async () => {
+            await expect(page.getByTestId(FooterLocators.FOOTER_PLAN_SECTION).getByRole('listitem').filter({ hasText: 'Standard'})).toBeVisible();
+        });
+        await test.step(`Click on the Standard Plan card.`, async () => {
+            await page.getByTestId(FooterLocators.FOOTER_PLAN_SECTION).getByRole('listitem').filter({ hasText: 'Standard'}).click();
+        });
+        await test.step(`Check the site opens the correct page.`, async () => {
+            await expect(page).toHaveURL(/.*a-radically-better-account/);
+        });
     });
+
+    test("Test the Plus Plan card", async ({ page }) => {
+        await test.step(`Navigate to ${url}.`, async () => {
+            await page.goto('/');
+            await page.getByRole('button', {name: "Accept all"}).click();
+        });
+        await test.step('Check the Plus Plan card in footer', async () => {
+            await expect(page.getByTestId(FooterLocators.FOOTER_PLAN_SECTION).getByRole('listitem').filter({ hasText: 'Plus'})).toBeVisible();
+        });
+        await test.step(`Click on the Plus Plan card.`, async () => {
+            await page.getByTestId(FooterLocators.FOOTER_PLAN_SECTION).getByRole('listitem').filter({ hasText: 'Plus'}).click();
+        });
+        await test.step(`Check the site opens the correct page.`, async () => {
+            await expect(page).toHaveURL(/.*revolut-plus/);
+        });
+    });
+
+    test("Test the Premium Plan card", async ({ page }) => {
+        await test.step(`Navigate to ${url}.`, async () => {
+            await page.goto('/');
+            await page.getByRole('button', {name: "Accept all"}).click();
+        });
+        await test.step('Check the Premium Plan card in footer', async () => {
+            await expect(page.getByTestId(FooterLocators.FOOTER_PLAN_SECTION).getByRole('listitem').filter({ hasText: 'Premium'})).toBeVisible();
+        });
+        await test.step(`Click on the Premium Plan card.`, async () => {
+            await page.getByTestId(FooterLocators.FOOTER_PLAN_SECTION).getByRole('listitem').filter({ hasText: 'Premium'}).click();
+        });
+        await test.step(`Check the site navigates to the correct page.`, async () => {
+            await expect(page).toHaveURL(/.*revolut-premium/);
+        });
+    });
+
+    test("Test the Metal card", async ({ page }) => {
+        await test.step(`Navigate to ${url}.`, async () => {
+            await page.goto('/');
+            await page.getByRole('button', {name: "Accept all"}).click();
+        });
+        await test.step('Check the Metal card in footer', async () => {
+            await expect(page.getByTestId(FooterLocators.FOOTER_PLAN_SECTION).getByRole('listitem').filter({ hasText: 'Metal'})).toBeVisible();
+        });
+        await test.step(`Click on the Metal card.`, async () => {
+            await page.getByTestId(FooterLocators.FOOTER_PLAN_SECTION).getByRole('listitem').filter({ hasText: 'Metal'}).click();
+        });
+        await test.step(`Check the site navigates to the correct page.`, async () => {
+            await expect(page).toHaveURL(/.*metal/);
+        });
+    });
+
+    test("Test the Ultra card", async ({ page }) => {
+        await test.step(`Navigate to ${url}.`, async () => {
+            await page.goto('/');
+            await page.getByRole('button', {name: "Accept all"}).click();
+        });
+        await test.step('Check the Ultra card in footer', async () => {
+            await expect(page.getByTestId(FooterLocators.FOOTER_PLAN_SECTION).getByRole('listitem').filter({ hasText: 'Ultra'})).toBeVisible();
+        });
+        await test.step(`Click on the Ultra card.`, async () => {
+            await page.getByTestId(FooterLocators.FOOTER_PLAN_SECTION).getByRole('listitem').filter({ hasText: 'Ultra'}).click();
+        });
+        await test.step(`Check the site navigates to the correct page.`, async () => {
+            await expect(page).toHaveURL(/.*ultra-plan/);
+        });
+    });
+});
+
+test.describe('Footer - Legal', async () => {
+
+    test('Test the legal section in footer', async ({page}) => {
+        await test.step(`Navigate to ${url}.`, async () => {
+            await page.goto('/');
+            await page.getByRole('button', {name: "Accept all"}).click();
+        });
+        await test.step('Check the Plan section in footer.', async () => {
+            await expect(page.getByTestId(FooterLocators.FOOTER_LEGAL_SECTION)).toBeVisible();
+        });
+    })
+});
+
+test.describe('Footer - Qucik links', async () => {
+
+    test('Test the quick link section in footer', async ({page}) => {
+        await test.step(`Navigate to ${url}.`, async () => {
+            await page.goto('/');
+            await page.getByRole('button', {name: "Accept all"}).click();
+        });
+        await test.step('Check the Plan section in footer.', async () => {
+            await expect(page.getByTestId(FooterLocators.FOOTER_QUICK_LINKS)).toBeVisible();
+        });
+    })
 });
